966fa138e09457de3582424a5b58586ee0d1cf4f
Tong Leiming <tongleiming@daling.com>
2020-04-21 11:00:52 +0800

    Initial Commit

---
+++ XCPromise/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  XCPromise
+//
+//  Created by tongleiming on 2020/4/21.
+//  Copyright © 2020 tongleiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ XCPromise/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XCPromise
+//
+//  Created by tongleiming on 2020/4/21.
+//  Copyright © 2020 tongleiming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
